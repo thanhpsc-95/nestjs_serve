0ab73398df404165b6b8201da8593815ac6d96d9
thanhd <thanhd19tl@gmail.com>
2021-07-30 23:22:12 +0700

    add Cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT || 3000);
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
 }
 bootstrap();
